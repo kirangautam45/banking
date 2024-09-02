09832feff9b4f2873d52f8de0e08e15a5d884fd7
kirangautam45 <kirangautam225@gmail.com>
2024-09-02 10:50:14 +0545

    Substring and empty space avilabe added

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -9,9 +9,9 @@ import { authFormSchema } from '@/lib/utils'
 const formSchema = authFormSchema('sign-up')
 
 interface CustomInput {
-  control: Control<z.infer<typeof formSchema>>,
-  name: FieldPath<z.infer<typeof formSchema>>,
-  label: string,
+  control: Control<z.infer<typeof formSchema>>
+  name: FieldPath<z.infer<typeof formSchema>>
+  label: string
   placeholder: string
 }
 
@@ -21,20 +21,18 @@ const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
       control={control}
       name={name}
       render={({ field }) => (
-        <div className="form-item">
-          <FormLabel className="form-label">
-            {label}
-          </FormLabel>
-          <div className="flex w-full flex-col">
+        <div className='form-item'>
+          <FormLabel className='form-label'>{label}</FormLabel>
+          <div className='flex w-full flex-col'>
             <FormControl>
-              <Input 
+              <Input
                 placeholder={placeholder}
-                className="input-class"
+                className='input-class'
                 type={name === 'password' ? 'password' : 'text'}
                 {...field}
               />
             </FormControl>
-            <FormMessage className="form-message mt-2" />
+            <FormMessage className='form-message mt-2' />
           </div>
         </div>
       )}
